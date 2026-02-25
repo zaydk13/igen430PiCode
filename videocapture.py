69b3be8e95aaa8ea4804980370ef4056d4c6b108
zaydk13 <146591232+zaydk13@users.noreply.github.com>
2026-02-24 21:48:45 -0800

    folder clearing changed

--- videocapture.py
+++ videocapture.py
@@ -9,9 +9,18 @@ import cv2
 import os
 import shutil
 
-# Clear image_send folder
-if os.path.exists('image_send'):
-    shutil.rmtree('image_send')
+def empty_folder(folder_path):
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            # If it's a file or a symbolic link, use os.unlink (or os.remove)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            # If it's a directory, use shutil.rmtree to wipe its contents too
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
 
 # GPIO setup
 gpio.setmode(gpio.BCM)
